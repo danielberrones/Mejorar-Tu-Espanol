ea9f5cd694cebbcc14e4a9a3641587a327d9b6a8
danielberrones <daniel.a.berrones@gmail.com>
2023-04-25 00:11:11 -0500

    refactored

---
+++ interactuar.py
@@ -0,0 +1,61 @@
+# Lista de preguntas
+preguntas = [
+    "¿Te gustó eso?",
+    "¿Te encantó eso?",
+    "¿Te fascinó eso?",
+    "¿Te impresionó eso?",
+    "¿Lo disfrutaste?",
+    "¿Te pareció interesante?",
+    "¿Te resultó emocionante?",
+    "¿Te pareció atractivo?",
+    "¿Te gustó mucho eso?",
+    "¿Te dejó satisfecho/a?",
+    "¿Te pareció genial?",
+    "¿Te cautivó?",
+    "¿Te gustó de verdad eso?",
+    "¿Te maravilló eso?",
+    "¿Te encantó por completo eso?",
+    "¿Te emocionó mucho eso?",
+    "¿Te pareció increíble?",
+    "¿Te sorprendió gratamente eso?",
+    "¿Te dejó sin palabras?",
+    "¿Te fascinó de principio a fin?",
+    "¿Te gustó mucho más de lo que esperabas?",
+    "¿Te enamoraste de eso?",
+    "¿Te hizo sentir feliz?",
+    "¿Te dejó con ganas de más?",
+    "¿Te impactó positivamente eso?",
+    "¿Te resultó fascinante en todos los sentidos?",
+    "¿Te llenó de satisfacción eso?",
+    "¿Te conquistó por completo eso?",
+    "¿Te hizo sonreír?",
+    "¿Te impresionó de forma especial eso?",
+    "¿Te hizo sentir emociones intensas?",
+    "¿Te encantó hasta el último detalle?",
+    "¿Te sorprendió gratamente eso?",
+    "¿Te llegó al corazón?",
+    "¿Te hizo pasar un buen momento?",
+    "¿Te hizo pensar y reflexionar?",
+    "¿Te atrapó desde el principio?",
+    "¿Te resultó adictivo/a?",
+    "¿Te gustó más de lo que pensabas?",
+    "¿Te generó admiración eso?",
+    "¿Te hizo disfrutar mucho?",
+    "¿Te dejó una impresión duradera?",
+    "¿Te hizo sentir inspirado/a?",
+    "¿Te pareció único/a?",
+    "¿Te cautivó desde la primera vez?",
+    "¿Te hizo sentir identificado/a?",
+    "¿Te hizo emocionarte?",
+    "¿Te encantó el enfoque que tuvo eso?",
+    "¿Te dejó con una sonrisa en el rostro?",
+    "¿Te hizo querer repetir la experiencia?"
+]
+
+# Bucle while para preguntar y obtener respuestas
+indice = 0
+while indice < len(preguntas):
+    pregunta = preguntas[indice]
+    respuesta = input(f"{pregunta}: ")
+    indice += 1
+
